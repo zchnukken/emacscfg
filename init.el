9bce240f830ebd26dc381b2aad84c4723c2442ed
Anton <zchnukken@gmail.com>
2017-06-13 07:58:04 +0200

    first commit

---
+++ init.el
@@ -0,0 +1,21 @@
+(tool-bar-mode 0)
+(add-to-list 'custom-theme-load-path "~/.emacs.d/themes/")
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("4528fb576178303ee89888e8126449341d463001cb38abe0015541eb798d8a23" default))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+(load-theme ' zenburn t)
+
+
